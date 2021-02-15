fca418c442adbe0f6c019b65149f392a399794a0
shakilkhan1414 <khan.shakil.1414@gmail.com>
2021-02-15 13:44:05 -0500

    2nd

--- MID_LAB_TASK_PHP_04/1.php
+++ MID_LAB_TASK_PHP_04/1.php
@@ -3,7 +3,7 @@
     $width=5;
     $area= $length*$width;
     $perimeter=2*($length+$width);
-    echo "Area: ".$area;
+    echo "Area= ".$area;
     echo "<br>";
-    echo "Perimeter: ".$perimeter;
+    echo "Perimeter= ".$perimeter;
 ?>
---
+++ MID_LAB_TASK_PHP_04/2.php
@@ -0,0 +1,5 @@
+<?php 
+    $amount=100;
+    $vat=($amount*15)/100;
+    echo "Vat= ".$vat;
+?>
