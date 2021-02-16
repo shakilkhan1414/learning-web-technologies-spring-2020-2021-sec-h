0a0279fe1e3ddfff9c7d4e9cf9b1d03a06d60a8e
shakilkhan1414 <69055926+shakilkhan1414@users.noreply.github.com>
2021-02-16 02:42:12 -0500

    task2.3
    task2.3

---
+++ MID_LAB_TASK_PHP_05/TASK_2/TASK_C/index.php
@@ -0,0 +1,29 @@
+
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Email</title>
+</head>
+<body>
+
+	<form method="POST">
+		<fieldset style="width: 300px;">
+			<legend>Email</legend>
+			Email: <input type="email" name="myname" value="  <?php
+	if(isset($_POST['submit']))
+	{
+        if(isset($_POST['myname']))
+        {
+            echo $_POST['myname'];
+        }
+    }   ?> ">  
+    
+            <br>
+            <br>
+            <hr>
+				<input type="submit" name="submit" value="Submit">
+		</fieldset>
+	</form>
+</body>
+</html>
