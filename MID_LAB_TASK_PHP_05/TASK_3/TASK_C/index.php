55d87fe3c480569fc5812d909406c44114761bc8
shakilkhan1414 <khan.shakil.1414@gmail.com>
2021-02-16 10:28:59 -0500

    task6.3

--- MID_LAB_TASK_PHP_05/TASK_3/TASK_C/index.php
+++ MID_LAB_TASK_PHP_05/TASK_3/TASK_C/index.php
@@ -1,5 +1,3 @@
-
-
 <!DOCTYPE html>
 <html>
 <head>
@@ -10,14 +8,11 @@
 	<form method="POST">
 		<fieldset style="width: 300px;">
 			<legend>Date</legend>
-			Date: <input type="date" name="myname" value="  <?php
-	if(isset($_POST['submit']))
-	{
-        if(isset($_POST['myname']))
-        {
-            echo $_POST['myname'];
-        }
-    }   ?> ">  
+			Date: <input type="date" name="myname" value="<?php
+				if(isset($_POST['submit']))
+				{
+						echo $_POST['myname'];
+				}   ?>">  
     
             <br>
             <br>
