2920cb9f22a239ce6c75ee2e05defdf53c1e941d
shakilkhan1414 <69055926+shakilkhan1414@users.noreply.github.com>
2021-02-16 02:50:31 -0500

    task3.3
    task3.3

---
+++ MID_LAB_TASK_PHP_05/TASK_3/TASK_C/index.php
@@ -0,0 +1,29 @@
+
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Date</title>
+</head>
+<body>
+
+	<form method="POST">
+		<fieldset style="width: 300px;">
+			<legend>Date</legend>
+			Date: <input type="date" name="myname" value="  <?php
+	if(isset($_POST['submit']))
+	{
+        if(isset($_POST['myname']))
+        {
+            echo $_POST['myname'];
+        }
+    }   ?> ">  
+    
+            <br>
+            <br>
+            <hr>
+				<input type="submit" name="submit" value="Submit">
+		</fieldset>
+	</form>
+</body>
+</html>
