a5a7df4b016639b7e926af95a5484e3cf4c3bde5
shakilkhan1414 <khan.shakil.1414@gmail.com>
2021-03-03 02:56:11 +0600

    user_list-finish

--- MID_LAB_TASK_JSON_08/view/user_list.php
+++ MID_LAB_TASK_JSON_08/view/user_list.php
@@ -3,6 +3,13 @@
 	include('header.php');
 ?>
 
+<style>
+	td{
+		padding: 5px 20px;
+	}
+
+</style>
+
 	<div id="page_title">
 		<h1>User List</h1>
 	</div>
@@ -11,42 +18,47 @@
 		<a href="home.php"> Back</a> |
 		<a href="../controller/logout.php"> logout</a>	
 	</div>
-
+	<br>
 	<div id="main_content">
-		<table border="1">
+		<table border="1" style="border-collapse: collapse;">
 			<tr>
 				<td>ID</td>
 				<td>NAME</td>
 				<td>EMAIL</td>
 				<td>ACTION</td>
 			</tr>
-			<tr>
-				<td>1</td>
-				<td>alamin</td>
-				<td>alamin@gmail.com</td>
-				<td>
-					<a href="edit.php?id=1">EDIT</a> |
-					<a href="delete.php?id=1">DELETE</a> 
-				</td>
-			</tr>
-			<tr>
-				<td>2</td>
-				<td>xyz</td>
-				<td>xyz@gmail.com</td>
-				<td>
-					<a href="edit.php?id=2">EDIT</a> |
-					<a href="delete.php?id=2">DELETE</a> 
-				</td>
-			</tr>
-			<tr>
-				<td>3</td>
-				<td>abc</td>
-				<td>abc@gmail.com</td>
-				<td>
-					<a href="edit.php?id=3">EDIT</a> |
-					<a href="delete.php?id=3">DELETE</a> 
-				</td>
-			</tr>
+
+			<?php 
+			
+			$myfile = fopen('../controller/list.json', 'r');
+			$data = fread($myfile, filesize('../controller/list.json'));
+
+			$user = json_decode($data, true);
+			
+
+			for ($i=0; $i < sizeof($user); $i++) {
+
+				$id=$user[$i]['id'];
+				$username=$user[$i]['username'];
+				$email=$user[$i]['email'];
+				
+
+				echo "
+				<tr>
+					<td> $id </td>
+					<td> $username </td>
+					<td> $email </td> 
+					<td>
+						<a href='edit.php?id=$id'>EDIT</a> |
+						<a href='delete.php?id=$id'>DELETE</a> 
+					</td>
+				</tr>		
+				";
+			}
+
+			?>
+			
+
 		</table>
 	</div>
 
