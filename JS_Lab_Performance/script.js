57b5be990479bd5f7db4f286a1af3d7cb11dd684
shakilkhan1414 <khan.shakil.1414@gmail.com>
2021-04-06 12:18:23 +0600

    js

--- JS_Lab_Performance/script.js
+++ JS_Lab_Performance/script.js
@@ -1,11 +1,11 @@
-
-  function validate() {
+function validate() {
     const name= document.getElementById('name').value;
     if (name=="") {
         document.getElementById('msg').innerHTML="Name cannot be empty!";
         return false;
     } else {
-        if(WordCount(name)<2){
+        var words = name.split(" ").length;
+        if(words<2){
             document.getElementById('msg').innerHTML="Name is too short!";
             return false;
         }
